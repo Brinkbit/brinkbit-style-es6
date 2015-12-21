3f4467940ea55bd7f74a3f7339b26853daba4be9
Justin Livi <justin@brinkbit.com>
2015-12-20 19:30:25 -0500

    Fix relative paths
    Relative paths for copying files were wrong in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const validate = require( 'git-validate' );
-validate.copy( '.eslintrc', '.eslintrc' );
-validate.copy( 'scripts/eslint-pre-commit', 'scripts/eslint-pre-commit' );
+validate.copy( '../.eslintrc', '.eslintrc' );
+validate.copy( '../scripts/eslint-pre-commit', 'scripts/eslint-pre-commit' );
 validate.installScript( 'eslint-pre-commit', './scripts/eslint-pre-commit' );
 validate.configureHook( 'pre-commit', ['eslint-pre-commit']);
